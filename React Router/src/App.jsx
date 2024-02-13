58b84d7c2da4df7b399e2ca502e99c145fcc6d1b
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2024-02-13 13:32:25 -0800

    complete code

--- React Router/src/App.jsx
+++ React Router/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route, Link} from 'react-router-dom';
 import Red from './component/Red';
 import Blue from './component/Blue';
+import Home from './component/Home';
 
 function App() {
   
@@ -10,11 +11,13 @@ function App() {
     <>
       <div id="container">
       <div id="navbar">
+        <Link to={"/"}>Home</Link>
         <Link to={"/blue"}>Blue</Link>
         <Link to={"/red"}>Red</Link>
       </div>
       <div id="main-section">
         <Routes>
+          <Route path='/' element = {<Home></Home>} />
           <Route path='/blue' element={<Blue></Blue>} />
           <Route path='/red' element={<Red></Red>} />
         </Routes>
